41c25bddbe48b4466d52d1863deef0f22de2c4c8
useonguk <ysu0219@gmail.com>
2023-08-16 08:44:07 +0900

    default set

---
+++ src/components/Fishing/river.tsx
@@ -0,0 +1,9 @@
+import * as S from "./styleRiver";
+
+export default () => {
+  return (
+    <S.RiverBackground>
+      <S.InventoryBox></S.InventoryBox>
+    </S.RiverBackground>
+  );
+};
---
+++ src/components/Fishing/styleRiver.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const RiverBackground = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #00e0ff;
+`;
+
+export const InventoryBox = styled.div`
+  max-height: 18em;
+  min-height: 12em;
+  max-width: 30em;
+  min-width: 20em;
+  width: 40vw;
+  height: 35vh;
+  background-color: white;
+`;
