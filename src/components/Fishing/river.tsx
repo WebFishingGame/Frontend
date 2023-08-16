0f91041dc8360986ea3ccedbf7975fe6bac65f2b
useonguk <ysu0219@gmail.com>
2023-08-16 09:44:48 +0900

    ADD : inventory

--- src/components/Fishing/river.tsx
+++ src/components/Fishing/river.tsx
@@ -1,9 +1,15 @@
+import React, { useState } from "react";
 import * as S from "./styleRiver";
 
 export default () => {
+  const [test, setTest] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   return (
     <S.RiverBackground>
-      <S.InventoryBox></S.InventoryBox>
+      <S.InventoryBoxes>
+        {test.map((t) => {
+          return <S.InventoryBox>{t}</S.InventoryBox>;
+        })}
+      </S.InventoryBoxes>
     </S.RiverBackground>
   );
 };
--- src/components/Fishing/styleRiver.ts
+++ src/components/Fishing/styleRiver.ts
@@ -4,14 +4,28 @@ export const RiverBackground = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: #00e0ff;
+  padding-top: 1em;
 `;
 
-export const InventoryBox = styled.div`
+export const InventoryBoxes = styled.div`
   max-height: 18em;
   min-height: 12em;
   max-width: 30em;
   min-width: 20em;
   width: 40vw;
-  height: 35vh;
+  height: 28vh;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  margin-left: 1em;
+`;
+
+export const InventoryBox = styled.div`
+  max-height: 9em;
+  min-height: 6em;
+  max-width: 6em;
+  min-width: 4em;
+  width: 8vw;
+  height: 14vh;
+  border: 1px solid black;
   background-color: white;
 `;
